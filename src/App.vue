ba346ffe53a8fe3b9dadadaef83cf038a1571dd3
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-01-07 09:24:28 -0300

    Configurei o site para esticar-se só até o tamanho de um tablet, mas ainda falta os cookies

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,46 @@ import database from './assets/database.json';
 </script>
 
 <template>
-  <div class="p-3">
-    <header>
-      <h1 class="text-center mb-3 display-1">Detetive</h1>
-      <h2 class="text-center mb-3 display-5">Ficha Eletrônica</h2>
-    </header>
+  <div class="p-3 d-flex flex-column align-items-center">
+    <div class="centralizer-wrapper">
+      <header>
+        <h1 class="text-center mb-3 display-1">Detetive</h1>
+        <h2 class="text-center mb-3 display-5">Ficha Eletrônica</h2>
+      </header>
+      <div class="mobile-screens">
+        <Category
+          v-for="categoria in database.categorias"
+          :key="categoria.idCategoria"
+          :idCategoria="categoria.idCategoria"
+          :categoria="categoria.categoria"
+          :conteudo="categoria.conteudo"
+        />
 
-    <Category
-      v-for="categoria in database.categorias"
-      :key="categoria.idCategoria"
-      :idCategoria="categoria.idCategoria"
-      :categoria="categoria.categoria"
-      :conteudo="categoria.conteudo"
-    />
+        <div class="mt-5">
+          <ResetButton />
+        </div>
+      </div>
 
-    <div class="mt-5">
-      <ResetButton />
+      <hr class="my-3" />
+
+      <footer class="mt-2 text-center">
+        <p>
+          Este site utiliza cookies somente para armazenar os dados do jogo, portanto, permita o uso
+          de cookies para uma boa experiência.
+        </p>
+        <p>
+          Desenvolvido por Giovanni Vasconcelos de Medeiros. Acesse meu site
+          <a href="https://vasconcelos-giovanni.github.io/giovanni-vasconcelos">clicando aqui</a>.
+        </p>
+      </footer>
     </div>
-
-    <hr class="my-3" />
-
-    <footer class="mt-2 text-center">
-      <p>
-        Este site utiliza cookies somente para armazenar os dados do jogo, portanto, permita o uso
-        de cookies para uma boa experiência.
-      </p>
-      <p>
-        Desenvolvido por Giovanni Vasconcelos de Medeiros. Acesse meu site
-        <a href="https://vasconcelos-giovanni.github.io/giovanni-vasconcelos">clicando aqui</a>.
-      </p>
-    </footer>
   </div>
 
   <!-- <RouterView /> -->
 </template>
+
+<style>
+.centralizer-wrapper {
+  max-width: 720px;
+}
+</style>
