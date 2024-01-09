64029f57f6480b0186f8b422d9d58622a1ff1d07
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-01-09 01:57:42 -0300

    Save cookies

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, reactive, ref, watch } from 'vue';
+import Cookies from 'js-cookie';
 
 const props = defineProps({
   idCategoria: {
@@ -16,7 +17,62 @@ const props = defineProps({
   },
 });
 
+const idsPrefixes = {
+  select: 'select_' + props.categoria,
+  checkbox: 'checkbox_' + props.categoria,
+  input: 'input_' + props.categoria,
+};
+
 const selectedOption = ref('?');
+Cookies.set(idsPrefixes.select, selectedOption.value);
+
+watch(selectedOption, () => {
+  // Select
+  Cookies.set(idsPrefixes.select, selectedOption.value);
+});
+
+const checkboxes = reactive({});
+const inputs = reactive({});
+
+onMounted(() => {
+  // Checkboxes
+  props.conteudo.forEach(({ idItem }) => {
+    const checkboxId = idsPrefixes.checkbox + idItem;
+    checkboxes[checkboxId] = false; // Set initial value to false
+    Cookies.set(checkboxId, checkboxes[checkboxId]);
+  });
+
+  // Inputs
+  props.conteudo.forEach(({ idItem }) => {
+    const inputId = idsPrefixes.input + idItem;
+    inputs[inputId] = ''; // Set initial value to empty
+    Cookies.set(inputId, inputs[inputId]);
+  });
+
+  watch(
+    checkboxes,
+    (newCheckboxes) => {
+      for (const checkbox in newCheckboxes) {
+        Cookies.set(checkbox, newCheckboxes[checkbox]);
+      }
+    },
+    { deep: true },
+  );
+
+  watch(
+    inputs,
+    (newInputs) => {
+      for (const input in newInputs) {
+        Cookies.set(input, newInputs[input]);
+      }
+    },
+    { deep: true },
+  );
+});
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 const finalGuessMade = (id) => {
   return id != selectedOption.value && selectedOption.value != '?' ? true : false;
@@ -25,10 +81,14 @@ const finalGuessMade = (id) => {
 
 <template>
   <section class="col-12">
-    <h3 class="fs-2">{{ props.categoria }}</h3>
+    <h3 class="fs-2">{{ capitalizeFirstLetter(props.categoria) }}</h3>
 
-    <label class="form-check-label" for="characterSelect"> Palpite final </label>
-    <select class="form-select mb-3 fw-bold btn btn-primary" v-model="selectedOption">
+    <label class="form-check-label" :for="idsPrefixes.select"> Palpite final </label>
+    <select
+      class="form-select mb-3 fw-bold btn btn-primary"
+      v-model="selectedOption"
+      :id="'select_' + props.categoria"
+    >
       <option value="?" selected>?</option>
       <option v-for="{ idItem, item } in conteudo" :key="idItem" :value="idItem">
         {{ item }}
@@ -36,13 +96,24 @@ const finalGuessMade = (id) => {
     </select>
     <div>
       <div class="mb-3" v-for="{ idItem, item } in conteudo" :key="idItem">
-        <input class="form-check-input" type="checkbox" :id="idItem" />
-        <label class="form-check-label fw-bold ms-1" :for="idItem">
+        <input
+          class="form-check-input"
+          type="checkbox"
+          :id="idsPrefixes.checkbox + idItem"
+          v-model="checkboxes[`${idsPrefixes.checkbox}${idItem}`]"
+        />
+        <label class="form-check-label fw-bold ms-1" :for="idsPrefixes.checkbox + idItem">
           <span class="text-danger" v-if="finalGuessMade(idItem)">{{ item }}</span>
           <span v-else>{{ item }}</span>
         </label>
         <div class="d-flex justify-content-end">
-          <input type="text" class="form-control ms-3" placeholder="Comentário" />
+          <input
+            type="text"
+            class="form-control ms-3"
+            placeholder="Comentário"
+            v-model="inputs[`${idsPrefixes.input}${idItem}`]"
+            :id="idsPrefixes.input + idItem"
+          />
         </div>
       </div>
     </div>
