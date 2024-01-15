7db46de8558c26e6cdea9769c2e53a4f77adc7f5
Giovanni Vasconcelos <vasconcelos.giovanni@hotmail.com>
2024-01-15 17:22:31 -0300

    feat: make cards for boards for HomeView

---
+++ src/components/BoardVersionCard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  coverName: {
+    required: true,
+    type: String,
+  },
+  routeName: {
+    required: true,
+    type: String,
+  },
+  title: {
+    required: true,
+    type: String,
+  },
+});
+
+const BOARDS_COVERS_PATH_PREFIX = '/src/assets/img/boards/';
+</script>
+
+<template>
+  <div class="card">
+    <img
+      :src="`${BOARDS_COVERS_PATH_PREFIX}${props.coverName}`"
+      class="card-img-top board-cover"
+      :alt="`Versão ${props.title}`"
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ props.title }}</h5>
+      <router-link :to="{ name: props.routeName }" class="btn btn-primary">Abrir</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board-cover {
+  max-height: 11.36rem;
+  max-width: 18rem;
+  object-fit: cover;
+  overflow: hidden;
+}
+
+.card {
+  width: 18rem;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
+import BoardVersionCard from './BoardVersionCard.vue';
 import Category from './Category.vue';
+import MainNavbar from './MainNavbar.vue';
 import ResetButton from './ResetButton.vue';
+import MainFooter from './MainFooter.vue';
 
-export { Category, ResetButton };
+export { BoardVersionCard, Category, MainFooter, MainNavbar, ResetButton };
